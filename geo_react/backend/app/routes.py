726047991dce034f33a54d2b1c722eea89d6c59a
Jordan Perry-Greene 24E <jperrygreene23@rlinux-cosc-257-node14.amherst.edu>
2023-11-14 17:15:49 -0500

    models are in

--- geo_react/backend/app/routes.py
+++ geo_react/backend/app/routes.py
@@ -2,7 +2,7 @@
 
 from flask import render_template
 from app import app
-from app.models import Student
+from app.models import Student, Location, Program, Student_Program, Program_Location, Participates_In, Is_About
 
 @app.route('/')
 def index():
