818908537b927e60c0ac47c08f9da394a176cb3c
Regina Deri 25 <rderi25@rlinux-cosc-257-node14.amherst.edu>
2023-11-14 18:38:32 -0500

    added test route to return sample data

--- geo_react/backend/app/routes.py
+++ geo_react/backend/app/routes.py
@@ -17,3 +17,25 @@ def test_student():
             'primary_reason':student.primary_reason,
             'language_proficiency':student.language_proficiency} for student in students]
     return jsonify(res)
+
+@app.route('/test_all')
+def test_all():
+    res = [{'program_name': 'Program 1',
+            'country':'Country 1',
+            'influencing_factors':'Factor 1',
+            'primary_language':'Language 1',
+            'amount_spent':'USD 1',
+            'extracurriculars':'Activity 1',
+            'recommendation_rating':'1',
+            'primary_reason':'Reason 1',
+            'major':'Major 1'},
+            {'program_name': 'Program 2',
+            'country':'Country 2',
+            'influencing_factors':'Factor 2',
+            'primary_language':'Language 2',
+            'amount_spent':'USD 2',
+            'extracurriculars':'Activity 2',
+            'recommendation_rating':'2',
+            'primary_reason':'Reason 2',
+            'major':'Major 2'}]
+    return jsonify(res)
