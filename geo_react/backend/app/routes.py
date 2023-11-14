8c8ae64bee122f288209990918c7f7ee80c05d0c
rderi <98278235+rderi@users.noreply.github.com>
2023-11-14 18:00:51 -0500

    Update routes.py

--- geo_react/backend/app/routes.py
+++ geo_react/backend/app/routes.py
@@ -1,10 +1,19 @@
 # app/routes.py
 
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
-from app.models import Student, Location, Program, Student_Program, Program_Location, Participates_In, Is_About
+from app.models import Student
 
 @app.route('/')
 def index():
     students = Student.query.all()
     return render_template('index.html', students=students)
+
+@app.route('/test_student')
+def test_student():
+    students = Student.query.all()
+    res = [{'student_email':student.student_email,
+            'major':student.major,
+            'primary_reason':student.primary_reason,
+            'language_proficiency':student.language_proficiency} for student in students]
+    return jsonify(res)
