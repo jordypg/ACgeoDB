18994d4732eaf71c15002bf348b7acbc76d96151
Regina Deri 25 <rderi25@rlinux-cosc-257-node14.amherst.edu>
2023-11-13 22:02:49 -0500

    added results table

--- geo_react/src/App.js
+++ geo_react/src/App.js
@@ -4,6 +4,13 @@ import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 
+function removePreviousResults(){
+  let previousResults = document.getElementById("search_results");
+  if(previousResults != null){
+    previousResults.remove();
+  }
+}
+
 function SearchField({number}){
   const selectId = "fieldSelect" + number;
   const textId = "fieldValue" + number;
@@ -11,8 +18,14 @@ function SearchField({number}){
     <>
       <select name={selectId} id={selectId}>
         <option value="progName">Program name</option>
-        <option value="country">Country</option>
-        <option value="partMajor">Participant major</option>
+        <option value="progCountry">Country</option>
+        <option value="decFact">Deciding Factor for Program</option>
+        <option value="primLang">Primary Language Spoken</option>
+        <option value="estCost">Estimated Cost Spent</option>
+        <option value="primLang">Extracurricular Activities</option>
+        <option value="primLang">Recommendation Rating</option>
+        <option value="primReas">Primary Reason for Study Abroad</option>
+        <option value="partMajor">Major</option>
       </select>
       <input id={textId} type="text"/> <br></br>
     </>
@@ -21,7 +34,7 @@ function SearchField({number}){
 
 function AddFieldsButton({handleClick}) {
   
-
+  removePreviousResults();
   return (
     <button onClick={handleClick}>
       Add field
@@ -31,29 +44,64 @@ function AddFieldsButton({handleClick}) {
 
 function SearchButton({handleClick, values}){
   return (
-    <button onclick={handleClick}>
+    <button onClick={handleClick}>
       Search
     </button>
   )
 }
 
-function QueryResults({entries}){
-  const [columnHeaders, setColumnHeaders] = useState([])
-  const [results, setResults] = useState([])
 
+
+function QueryResults({entries}){
+  const [columnHeaders, setColumnHeaders] = useState(["Program name", "Country", "Deciding Factor for Program", "Primary Language Spoken", "Estimated Cost Spent", "Extracurricular Activities", "Recommendation Rating", "Primary Reason for Study Abroad", "Major"]);
+  let rows = [];
+
+  //Add column headers to table
+  let headerCell = [];
+  for(var i = 0; i < columnHeaders.length; i++){
+    let cellID = `header${i}`;
+    headerCell.push(<td key={cellID} id={cellID}>{columnHeaders[i]}</td>)
+  }
+  rows.push(<tr key="header" id="header">{headerCell}</tr>)
+
+  //Add rows from entries
+  for (var i = 0; i < entries.length; i++){
+    let rowID = `data_row${i}`
+    let cell = []
+    for (var idx = 0; idx < entries[i].length; idx++){
+      let cellID = `data_cell${i}-${idx}`
+      cell.push(<td key={cellID} id={cellID}>{entries[i][idx]}</td>)
+    }
+    rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+  }
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col s12 board">
+          <table id="search_results">
+              <tbody>
+                {rows}
+              </tbody>
+            </table>
+        </div>
+      </div>
+    </div>
+  )
+    
 }
 
 function App() {
 
   const [searchFields, setSearchFields] = useState(1)
-
+  let test = [["AAA", "BBB", "CCC"], ["DDD", "EEE", "FFF"], ["GGG", "HHH", "III"]]
   function handleAddFieldsButtonClick() {
     setSearchFields(searchFields + 1)
     render(<SearchField number={searchFields}/>);
   }
 
   function handleSearchButtonClick(){
-
+    removePreviousResults();
+    render (<QueryResults entries = {test}/>)
   }
 
   return (
@@ -63,7 +111,7 @@ function App() {
     <SearchButton handleClick={handleSearchButtonClick}/>
     <br></br>
     <SearchField number={searchFields}/>
-      
+    
     </>
   );
 }
