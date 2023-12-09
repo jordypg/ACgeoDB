09c72eed2d7f06b3062c52f41a390b06fa4420ea
rderi <98278235+rderi@users.noreply.github.com>
2023-12-09 17:20:41 -0500

    merged branch

--- geo_react/backend/app/extractData.py
+++ geo_react/backend/app/extractData.py
@@ -4,8 +4,44 @@ from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from models import Student, Program, Student_Program
-# 
+from models import *
+
+
+def list_to_str(my_list):
+    ret = [str(element) for element in my_list]
+    return ret
+
+
+def extract_program_name(text):
+    # Find the index of the first opening parenthesis or comma
+    index = min([text.find(char) for char in [',', '('] if text.find(char) != -1], default=-1)
+
+    # If there's no opening parenthesis or comma, return the original string
+    if index == -1:
+        return text
+
+    # Otherwise, return the part of the string before the special character
+    return text[:index].strip()
+
+#Works because pandas assigns float nan value to empty cells, even if they're marked as text in Excel
+def isEmptyCell(cell):
+    return not isinstance(cell, str)
+
+#Removes empty cells from a list of cells
+def remove_empty_cells(cells):
+    return [cell for cell in cells if not isEmptyCell(cell)]
+
+
+#Handles the case when "Other" is selected for a question by returning only the answer from the "Please specify" question
+def handle_other_selection(primary, other):
+    
+    '''The selection for specifying an other answer is not uniform over the questions; 
+       it's either "Other" or "Other (Please specify)". So we have to use str.startswith()'''
+    if primary.strip().startswith("Other") \
+       and not isEmptyCell(other):
+        return other
+    else: 
+        return primary
 
 def extract_values(input_file):
     # Read the Excel file into a pandas DataFrame
@@ -35,123 +71,197 @@ def insert_db(entry_dict):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # insert_students_db(entry_dict, session)
-    insert_student_program_db(entry_dict, session)
+    insert_student(entry_dict, session)
+    insert_major(entry_dict, session)
+    insert_has_major(entry_dict, session)
+    insert_program(entry_dict, session)
+    insert_term(entry_dict, session)
+    insert_participates_in(entry_dict, session)
+    insert_personal_reflection(entry_dict, session)
+    insert_program_reflection(entry_dict, session)
+    insert_is_about_academic_factors(entry_dict, session)
+    insert_is_about_financial_factors(entry_dict, session)
+    insert_is_about_social_factors(entry_dict, session)
+    insert_location(entry_dict, session)
+    insert_hosted_in(entry_dict, session)
     session.close()
 
-def insert_program_db(entry_dict):
-    url =  'postgresql://postgres:geodb@localhost/project'
-    engine = create_engine(url)
-    connection = engine.connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
+def insert_student(entry_dict, session):
+    foo = Student(
+        student_email = entry_dict['student_email'],
+        primary_reason = entry_dict['primary_reason']
+    )
 
-    ### DEAL WTIH DIFFERENT PROGRAMS WITHIN A PROGRAM
-    '''
-    my_string = "example(text, more)"
-    substring = my_string.split(('(', ','))[0]
-    '''
+    session.add(foo)
+    session.commit()  
 
-    ### WORK ON FIXING THIS
-    program_list = ['DIS Copenhagen', 'Maastricht University', 'IES Abroad Milan', 'IES Abroad Rome', 'University of Oxford', 'Columbia Visting Students Program ']
-    country_list = ['Denmark', 'Netherlands', 'Italy', 'Italy', 'United Kingdom', 'United States']
-    for i,program in enumerate(program_list):
-        foo = Program(
-            program_name = program,
-            country = country_list[i]
+
+
+majors = set()
+def insert_major(entry_dict, session):
+    entry_majors = entry_dict['majors']
+    for major in entry_majors.split(val_separator):
+        if major not in majors:
+            foo = Major(
+                major_name = major
+            )
+            session.add(foo)
+            session.commit()
+
+
+def insert_has_major(entry_dict, session):
+    entry_majors = entry_dict['majors']
+    for major in entry_majors.split(val_separator):
+        foo = Has_Major(
+            student_email = entry_dict['student_email'],
+            major_name = major
         )
         session.add(foo)
         session.commit()
-    session.close()
 
-def list_to_str(my_list):
-    ret = [str(element) for element in my_list]
-    return ret
+program_ids = dict() #Dictionary mapping every program name to their program IDs
+next_program_id = 0
+def insert_program(entry_dict, session):
+    
+    program = entry_dict['program_name']
+
+    #If we haven't encountered this program yet...
+    if program not in program_ids:
+        #...assign a program ID to it...
+        program_ids[program] = next_program_id
+        next_program_id += 1
+        #...and add it to the DB.
+        foo = Program (
+            program_name = program
+        )
+        session.add(foo)
+        session.commit()
+
+terms = set()
+def insert_term(entry_dict, session):
+    term = entry_dict['term']
+    if term not in terms:
+        foo = Term(term_id = term)
+    session.add(foo)
+    session.commit()
 
 
-def extract_before_special_chars(text):
-    # Find the index of the first opening parenthesis or comma
-    index = min([text.find(char) for char in [',', '('] if text.find(char) != -1], default=-1)
-
-    # If there's no opening parenthesis or comma, return the original string
-    if index == -1:
-        return text
-
-    # Otherwise, return the part of the string before the special character
-    return text[:index].strip()
-
-def insert_student_program_db(entry_dict, session):
-    tmp = extract_before_special_chars(entry_dict['program_name'])
-
-    foo = Student_Program(
+def insert_participates_in(entry_dict, session):
+    foo = Participates_In(
         student_email = entry_dict['student_email'],
-        program_name = tmp,
-        term = entry_dict['term']
+        program_name = entry_dict['program_name'],
+        term_id = entry_dict['term']
     )
 
     session.add(foo)
     session.commit()
 
-def insert_student_db(entry_dict, session):
-    # print(entry)
-    # print(type(entry))
-    # print(entry_dict)
-    # print(entry)
-    # print(entry['majors'])
-    tmp = list_to_str(entry_dict['majors'])
-    print(tmp)
-    foo = Student(
+next_pr_id = 0
+def insert_personal_reflection(entry_dict, session):
+    foo = Personal_Reflection(
         student_email = entry_dict['student_email'],
-        major = tmp, ## INCLUDE ALL MAJORS LATER
-        primary_reason = entry_dict['primary_reason_for_abroad'],
-        language_proficiency = entry_dict['language_proficiency_before_after']
+        program_name = entry_dict['program_name'],
+        term_id = entry_dict['term'],
+        pr_id = next_pr_id,
+        goals_reflection = entry['goals_reflection'],
+        growth = entry['growth'],
+        challenges = entry['challenges'],
+        new_perspectives = entry['new_perspectives'],
+        language_proficiency_before = entry['language_proficiency_before'],
+        language_proficiency_after = entry['language_proficiency_after'],
     )
+    next_pr_id += 1
+    session.add(foo)
+    session.commit()
 
+def insert_program_reflection(entry_dict, session):
+    foo = Program_Reflection (
+        student_email = entry_dict['student_email'],
+        program_name = entry_dict['program_name'],
+        term_id = entry_dict['term'],
+        pgr_id = program_ids[entry_dict['program_name']],
+        recommendation_rating = entry['recommendation_rating'],
+        recommendation_comments = entry['recommendation_comments']
+    )
+    session.add(foo)
+    session.commit()
+
+def insert_is_about_academic_factors(entry_dict, session):
+    foo = Has_Academic_Factors (
+        pgr_id = program_ids[entry_dict['program_name']],
+        program_name = entry_dict['program_name'],
+        term_id = entry_dict['term'],
+        courses_taken = entry_dict['courses_taken'],
+        courses_type = entry_dict['courses_type'],
+        academic_exc_avail = entry_dict['academic_excursion_avail'],
+        academic_exc_rating = entry_dict['academic_exc_rating'],
+        academic_exc_comments = entry_dict['academic_exc_comments'],
+        influencing_factors = entry_dict['influencing_factors'],
+        orientation_description = entry_dict['orientation_description']
+        )
+    session.add(foo)
+    session.commit()
+
+def insert_is_about_financial_factors(entry_dict, session):
+    foo = Has_Financial_Factors (
+        pgr_id = program_ids[entry_dict['program_name']],
+        program_name = entry_dict['program_name'],
+        term_id = entry_dict['term'],
+        amount_spent = entry_dict['amount_spent'],
+        city_affordability = entry_dict['city_affordability'],
+        housing_acc = entry_dict['housing_acc'],
+        housing_acc_comments = entry_dict['housing_acc_comments']
+
+        )
+    session.add(foo)
+    session.commit()
+
+def insert_is_about_social_factors(entry_dict, session):
+    foo = Has_Social_Factors (
+        pgr_id = program_ids[entry_dict['program_name']],
+        program_name = entry_dict['program_name'],
+        term_id = entry_dict['term'],
+        extracurricals = entry_dict['extracurriculars'],
+        attitudes_diff = entry_dict['attitudes_diff'],
+        attitudes_diff_comments = entry_dict['attitudes_diff_comments'],
+        res_staff = entry_dict['res_staff'],
+        res_staff_comments = entry_dict['res_staff_comments'],
+        leisure_exc_avail = entry_dict['leisure_exc_avail'],
+        leisure_exc_rating = entry_dict['leisure_exc_rating'],
+        leisure_exc_comments = entry_dict['leisure_exc_comments']
+        )
+    session.add(foo)
+    session.commit()
+
+#A mapping of program names to their locations
+location_names = {'IES Abroad Milan': 'Milan',
+                  'IES Abroad Rome': 'Rome',
+                  'Maastricht University': 'Maastricht',
+                  'DIS Copenhagen': 'Copenhagen',
+                  'University of Oxford, Lady Margaret Hall': 'Oxford',
+                  'Columbia Visiting Students Program': 'New York, NY'}
+
+def insert_location(entry_dict, session):
+    foo = Location (
+        location_name = location_names[entry_dict['program_name']],
+        primary_lang_spoken = entry_dict['primary_lang_spoken'],
+        country = entry_dict['country']
+        )
+    session.add(foo)
+    session.commit()
+
+def insert_hosted_in(entry_dict, session):
+    foo = Hosted_In (
+        program_name = entry_dict['program_name'],
+        location_name = location_names[entry_dict['program_name']]
+        )
     session.add(foo)
     session.commit()    
-
-def insert_students_db(entry_dict, session):
-    # for entry in entry_list:
-    #     foo = Student (
-    #         student_email = entry['student_email'],
-    #         major = entry['majors'], ## INCLUDE ALL MAJORS LATER
-    #         primary_reason = entry['primary_reason_for_abroad'],
-    #         language_proficiency = entry['language_proficiency_before_after'] 
-    #     )
-    #     session.add(foo)
-    #     session.commit()
-    insert_student_db(entry_dict, session)
-
-'''
-# def insert_students_db():
-#     url =  'postgresql://postgres:geodb@localhost/project'
-#     engine = create_engine(url)
-#     connection = engine.connect()
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-
-#     Base = declarative_base()
-    
-
-#     class Student(Base):
-#         __tablename__ = 'student'
-#         student_email = Column(String(255), primary_key=True, unique=True, nullable=False)
-#         major = Column(String(100), nullable=False)
-#         primary_reason = Column(String(255), nullable=False)
-#         language_proficiency = Column(String(50), nullable=False)
-    
-#     foo = Student(
-#         student_email = 'foofoo',
-#         major = 'foo',
-#         primary_reason = 'foofoofoo',
-#         language_proficiency = 'foofoofoofoo'
-#     )
-    
-#     session.add(foo)
-#     session.commit()
-'''
+  
 
 if __name__ == "__main__":
+    val_separator = '; ' #Separator used when creating a single string from an entry with multiple values (e.g. 'courses_taken') 
+
     # Use the relative path for the Excel file
     excel_file_path = 'Study Away Eval S23 AY2223 for CS class.xlsx'
 
@@ -165,39 +275,51 @@ if __name__ == "__main__":
         entry = {}
         entry['student_email'] = obj[0]
         entry['courses_taken'] = obj[1:10]
-        entry['course_types'] = obj[10:28]
-        entry['primary_reason_for_abroad'] = obj[30]
-        entry['primary_language_spoken'] = obj[31:33]
-        entry['language_proficiency_before_after'] = obj[33]
-        entry['res_staff_availability'] = obj[36:38]
-        entry['housing_accomodation'] = obj[38:41]
-        entry['academic_excursion_availability'] = obj[41:45]
-        entry['leisure_excursion_availability'] = obj[45:49]
-        entry['amount_of_money_spent'] = obj[50]
-        entry['affordability'] = obj[51]
-        entry['extracurricular_participated'] = obj[52]
-        entry['reflection_on_goals'] = obj[55]
-        entry['personal_and_academic_growth'] = obj[75]
-        entry['challenges_of_experience'] = obj[76]
-        entry['new_persoectives_post_program'] = obj[77]
-        entry['factor_influencing_experience'] = obj[89:93]
-        entry['attitudes_different_from_us'] = obj[92:95]
-        entry['recommendation_rating'] = obj[95:97]
+        entry['courses_type'] = obj[10:28]
+        entry['primary_reason'] = obj[30]
+        entry['primary_lang_spoken'] = handle_other_selection(obj[31], obj[32])
+        entry['language_proficiency_before'] = obj[33]
+        entry['language_proficiency_after'] = obj[34]
+        entry['orientation_description'] = obj[35]
+        entry['res_staff'] = obj[36]
+        entry['res_staff_comments'] = obj[37]
+        entry['housing_acc'] = handle_other_selection(obj[38], obj[39])
+        entry['housing_acc_comments'] = obj[40]
+        entry['academic_exc_avail'] = obj[41]
+        entry['academic_exc_rating'] = obj[43]
+        entry['academic_exc_comments'] = obj[44]
+        entry['leisure_exc_avail'] = obj[45]
+        entry['leisure_exc_rating'] = obj[47]
+        entry['leisure_exc_comments'] = obj[48]
+        entry['amount_spent'] = obj[50]
+        entry['city_affordability'] = obj[51]
+        entry['extracurriculars'] = handle_other_selection(obj[52], obj[53])
+        entry['goals_reflection'] = obj[55]
+        entry['growth'] = obj[75]
+        entry['challenges'] = obj[76]
+        entry['new_perspectives'] = obj[77]
+        entry['influencing_factors'] = [handle_other_selection(obj[89], obj[90]), obj[91]]
+        entry['attitudes_diff'] = handle_other_selection(obj[92], obj[93])
+        entry['attitudes_diff_comments'] = obj[94]
+        entry['recommendation_rating'] = obj[95]
+        entry['recommendation_comments'] = obj[96]
         entry['country'] = obj[100]
         entry['majors'] = obj[101:104]
-        entry['program_name'] = obj[104]
+        entry['program_name'] = extract_program_name(obj[104])
         entry['term'] = obj[106]
-        entries.append(entry)
+
+        #Stringify data & handle empty responses
+        for key in entry:
+            val = entry[key]
+            #Replace single, empty cells with an empty string
+            if not isinstance(val, list) and isEmptyCell(val): 
+                val = ""
+            #If the value represents multiple cells, remove empty cells, and make a single string out of it 
+            elif isinstance(val, list):
+                val = remove_empty_cells(val)
+                val = val_separator.join(val)
+            entry[key] = val
     
-    # first_entry = extracted_objects[0]
-    # print((first_entry))
-    # insert_db(first_entry)
     for entry in (entries):
         insert_db(entry)
-        # print('sex')
-        # print(entry)
-    # insert_program_db()
-    # insert_db(extracted_objects)
-    # for i, entry in enumerate(entries):
-    #first_object = extracted_objects[0] if extracted_objects else None
-    #print(first_object['values'][0])
+    
