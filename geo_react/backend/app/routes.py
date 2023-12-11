ad80cc98478442db6916c6d7249c5e20498f02d4
Jordan Perry-Greene 24E <jperrygreene23@rlinux-cosc-257-node14.amherst.edu>
2023-12-10 22:42:41 -0500

    all cards route complete

--- geo_react/backend/app/routes.py
+++ geo_react/backend/app/routes.py
@@ -1,17 +1,7 @@
 # app/routes.py
-
+from sqlalchemy import text
 from flask import render_template, jsonify
 from app import app, db
-from app.models import Student, Student_Program, Program
-
-@app.route('/getAllStudents')
-def index():
-    students = db.session.query(Student).all()
-    res = [{'student_email':student.student_email,
-            'major':student.major,
-            'primary_reason':student.primary_reason,
-            'language_proficiency':student.language_proficiency} for student in students]
-    return jsonify(res)
 
 #/get_backside?pgr_id={PROGRAM ID}
 @app.route('/get_backside', methods=['GET'])
@@ -62,48 +52,41 @@ def get_backside():
             }]    
     return jsonify(res)
 
-@app.route('/test_all')
-def test_all():
-    res = [{'program_name': 'Program 1',
-            'country':'Country 1',
-            'influencing_factors':'Factor 1',
-            'primary_language':'Language 1',
-            'amount_spent':'USD 1',
-            'extracurriculars':'Activity 1',
-            'recommendation_rating':'1',
-            'primary_reason':'Reason 1',
-            'major':'Major 1'},
-            {'program_name': 'Program 2',
-            'country':'Country 2',
-            'influencing_factors':'Factor 2',
-            'primary_language':'Language 2',
-            'amount_spent':'USD 2',
-            'extracurriculars':'Activity 2',
-            'recommendation_rating':'2',
-            'primary_reason':'Reason 2',
-            'major':'Major 2'}]
-    return jsonify(res)
+#/get_backside?pgr_id={PROGRAM ID}
+@app.route('/get_all_cards', methods=['GET'])
+def get_all_cards():
+    query = '''
+    SELECT pr.pgr_id,
+           pr.student_email,
+           h.program_name AS program,
+           l.country,
+           MAX(CASE WHEN m.row_num = 1 THEN m.major_name ELSE '' END) AS major_1,
+           MAX(CASE WHEN m.row_num = 2 THEN m.major_name ELSE '' END) AS major_2,
+           MAX(CASE WHEN m.row_num = 3 THEN m.major_name ELSE '' END) AS major_3
+    FROM program_reflection pr
+    LEFT JOIN (
+        SELECT student_email, major_name,
+               ROW_NUMBER() OVER(PARTITION BY student_email ORDER BY major_name) AS row_num
+        FROM has_major
+    ) m ON pr.student_email = m.student_email
+    LEFT JOIN hosted_in h ON pr.program_name = h.program_name
+    LEFT JOIN location l ON h.location_name = l.location_name
+    GROUP BY pr.pgr_id, pr.student_email, h.program_name, l.country;
+    '''
+    result = db.session.execute(text(query))
+    
+    # Extracting required columns from the result
+    rows = [
+        {
+            'pgr_id': row.pgr_id,
+            'student_email': row.student_email,
+            'program': row.program,
+            'country': row.country,
+            'major_1': row.major_1,
+            'major_2': row.major_2,
+            'major_3': row.major_3
+        }
+        for row in result
+    ]
 
-@app.route('/getStudents', methods=['GET'])
-def getStudents():
-    students = (
-            db.session.query(Student.student_email)
-            .join(Student_Program, Student.student_email == Student_Program.student_email)
-            .filter(Student_Program.program_name == 'DIS Copenhagen')
-            .all()
-    )
-    res = [student.student_email for student in students]
-    print(res)
-    return jsonify([{'student_email' : student.student_email} for student in students])
-
-@app.route('/getPrograms', methods=['GET'])
-def getPrograms():
-    programs = (
-            db.session.query(Program)
-            .join(Student_Program, Program.program_name == Student_Program.program_name)
-            .join(Student, Student.student_email == Student_Program.student_email)
-            .filter(Student.major.ilike('%ECON%'))
-            .all()
-    )
-    print(programs)
-    return jsonify([{'program_name' : program.program_name} for program in programs])
+    return jsonify(rows)
