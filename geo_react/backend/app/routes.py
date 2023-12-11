7af8b297e63ff702f1efb2b322ed33157bdc17a2
rderi <98278235+rderi@users.noreply.github.com>
2023-12-10 22:25:51 -0500

    get_backside route

--- geo_react/backend/app/routes.py
+++ geo_react/backend/app/routes.py
@@ -13,6 +13,54 @@ def index():
             'language_proficiency':student.language_proficiency} for student in students]
     return jsonify(res)
 
+#/get_backside?pgr_id={PROGRAM ID}
+@app.route('/get_backside', methods=['GET'])
+def get_backside():
+    pgr_id = request.args['pgr_id']
+    backside_query = '''select distinct program_reflection.term_id, primary_reason, courses_taken, housing_acc, housing_acc_comments,
+                        academic_exc_rating, academic_exc_comments, leisure_exc_rating, leisure_exc_comments, city_affordability,
+                        amount_spent, attitudes_diff, attitudes_diff_comments, growth, challenges, new_perspectives
+                        from public.program_reflection
+                        join public.is_about_academic_factors
+                        on program_reflection.pgr_id = is_about_academic_factors.pgr_id
+                        join public.is_about_financial_factors
+                        on program_reflection.pgr_id = is_about_financial_factors.pgr_id
+                        join public.is_about_social_factors
+                        on program_reflection.pgr_id = is_about_social_factors.pgr_id
+                        join public.personal_reflection
+                        on program_reflection.student_email = personal_reflection.student_email
+                        and program_reflection.program_name = personal_reflection.program_name
+                        and program_reflection.term_id = personal_reflection.term_id
+                        join public.participates_in
+                        on program_reflection.student_email = participates_in.student_email
+                        and program_reflection.program_name = participates_in.program_name
+                        and program_reflection.term_id = participates_in.term_id
+                        join public.student
+                        on participates_in.student_email = student.student_email
+                        where program_reflection.pgr_id LIKE ''' + f"'{pgr_id}' ;"
+    
+
+    backside = db.session.execute(text(backside_query)).all()
+    backside = backside[0]._asdict()
+
+    res = [{'term_id' : backside['term_id'],
+            'primary_reason' : backside['primary_reason'],
+            'courses_taken' : backside['courses_taken'],
+            'housing_acc' : backside['housing_acc'],
+            'housing_acc_comments' : backside['housing_acc_comments'],
+            'academic_exc_rating' : backside['academic_exc_rating'],
+            'academic_exc_comments' : backside['academic_exc_comments'],
+            'leisure_exc_rating' : backside['leisure_exc_rating'],
+            'leisure_exc_comments' : backside['leisure_exc_comments'],
+            'city_affordability' : backside['city_affordability'],
+            'amount_spent' : backside['amount_spent'],
+            'attitudes_diff' : backside['attitudes_diff'],
+            'attitudes_diff_comments' : backside['attitudes_diff_comments'],
+            'growth' : backside['growth'],
+            'challenges' : backside['challenges'],
+            'new_perspectives' : backside['new_perspectives']
+            }]    
+    return jsonify(res)
 
 @app.route('/test_all')
 def test_all():
