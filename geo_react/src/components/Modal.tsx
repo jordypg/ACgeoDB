b6c62f6b2973ec524fae3d710ff800631b8793eb
Jun Ho Yoon 24 <jyoon23@rlinux-cosc-257-node14.amherst.edu>
2023-12-13 18:28:40 -0500

    more comments

--- geo_react/src/components/Modal.tsx
+++ geo_react/src/components/Modal.tsx
@@ -10,16 +10,18 @@ interface ModalProps {
 }
 
 // Styled component for the modal
-const ModalContainer = styled.div<{ show: boolean }>`
-  display: ${({ show }) => (show ? 'block' : 'none')};
+const ModalContainer = styled.div`
   position: fixed;
-  z-index: 1;
+  z-index: 1000; // Ensure it's above other elements
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
   overflow: auto;
-  background-color: rgba(0, 0, 0, 0.4);
+  background-color: rgba(0, 0, 0, 0.4); // Semi-transparent background
+  // Other styles you might want to include
+  padding: 20px;
+  box-sizing: border-box;
 `;
 
 const ModalContent = styled.div`
@@ -71,13 +73,27 @@ const Modal: React.FC<ModalProps> = ({ show, onClose, cardBacksideDetails }) => 
     return null
   }
   return (
-    <ModalContainer show>
+    <ModalContainer style={{ display: show ? 'block' : 'none' }}>
       <ModalContent>
         <CloseButton onClick={onClose}>&times;</CloseButton>
         {cardBacksideDetails ? (
           <div>
             <p>Academic Comments: {cardBacksideDetails.academic_exc_comments}</p>
             <p>Rating: {cardBacksideDetails.academic_exc_rating}</p>
+            <p>Amount Spent: {cardBacksideDetails.amount_spent}</p>
+            <p>Attitudes Different: {cardBacksideDetails.attitudes_diff}</p>
+            <p>Attitudes Different Comments: {cardBacksideDetails.attitudes_diff_comments}</p>
+            <p>Chalenges: {cardBacksideDetails.challenges}</p>
+            <p>City Affordability: {cardBacksideDetails.city_affordability}</p>
+            <p>Courses Taken: {cardBacksideDetails.courses_taken}</p>
+            <p>Growth: {cardBacksideDetails.growth}</p>
+            <p>Housing Accomodations: {cardBacksideDetails.housing_acc}</p>
+            <p>Housing Accomodations Comments: {cardBacksideDetails.housing_acc_comments}</p>
+            <p>Leisure Excursion Rating: {cardBacksideDetails.leisure_exc_rating}</p>
+            <p>Leisure Excursion Comments: {cardBacksideDetails.leisure_exc_comments}</p>
+            <p>New Perspectives: {cardBacksideDetails.new_perspectives}</p>
+            <p>Primary Reason: {cardBacksideDetails.primary_reason}</p>
+            <p>Term ID: {cardBacksideDetails.term_id}</p>
             {/* Render other fields similarly */}
           </div>
         ) : (
