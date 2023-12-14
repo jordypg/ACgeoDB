b835267d75852a0ed94c678fbfdcc542b1ca06a3
Jordy Perry-Greene <47393268+jordypg@users.noreply.github.com>
2023-12-14 14:53:15 -0500

    Update Header.tsx

--- geo_react/src/components/Header.tsx
+++ geo_react/src/components/Header.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import amherstLogo from './amherst-logo.png';
+
 
 const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
   background-color: purple;
   color: white;
   padding: 20px;
-  text-align: center;
   font-size: 24px;
   margin-bottom: 20px;
+
+  img {
+    width: 40px; /* Adjust image size as needed */
+    margin-right: 10px; /* Add spacing between image and text */
+  }
+
+  strong {
+    font-weight: bold;
+  }
 `;
 
 const Header: React.FC = () => {
-  return <HeaderContainer>GEO DB</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <img src={amherstLogo} alt="Logo" />
+      <strong>GEO DB</strong>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
+
+
+
+
